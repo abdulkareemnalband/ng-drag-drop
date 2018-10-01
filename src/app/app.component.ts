c2a529a097344299394fab5f68cb4ec23cf48991
AbdulKareem Nalband <abdulkareem.sn15@gmail.com>
2018-10-01 16:16:33 +0530

    ng drag drop soratble

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'drag-drop';
+    title = 'drag-drop';
+
+    verticelData = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6'];
+    horizontalData = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    mixedData = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6'];
+
+    dropped(source: string[], event: CdkDragDrop<string[]>) {
+        moveItemInArray(source, event.previousIndex, event.currentIndex);
+    }
 }
